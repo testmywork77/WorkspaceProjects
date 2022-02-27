8dcee2ab6186687d5ec86e91adf45f629aa94249
testmywork77 <testmywork77@gmail.com>
2022-02-27 09:13:45 +0000

    Add all the projects

---
+++ CPrograms/HelloWorld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+// To compile and compile C Programs 
+C:\CPrograms>gcc HelloSarayu.c -o HelloSarayu
+C:\CPrograms>HelloWorld
+*/
+void main()
+{
+	printf("Hello Sarayu!");
+}
---
+++ Cypress/cypress-sample/cypress/integration/0-test-scripts/sample.spec.js
@@ -0,0 +1,12 @@
+describe("First Test Suite", function(){
+
+    it("First Test", function(){
+        expect(true).to.equal(true)
+    })
+
+    it("Second Test", function(){
+        expect(true).to.equal(false);
+    })    
+
+})
+
