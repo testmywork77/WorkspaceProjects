24018ace864d7288bdbf98e9bba56d2812675c95
testmywork77 <testmywork77@gmail.com>
2022-02-27 21:16:33 +0000

    Java project added

---
+++ Java/java-maven-project/src/main/java/FilePath.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FilePath {
+    static String absolutePath;
+    static File file;
+    public static void main(String[] args){
+        FilePath filePath = new FilePath();
+
+        String path = "src/test/resources";
+        file = new File(path);
+        absolutePath = file.getAbsolutePath();
+        System.out.println("AbsolutePath Using java.io.File: " + absolutePath);
+
+        Path resourceDirectory = Paths.get("src","test","resources");
+        absolutePath = resourceDirectory.toFile().getAbsolutePath();
+        System.out.println("AbsolutePath Using Path: " + absolutePath);
+
+        String resourceName = "json/employee/employee.json";
+        ClassLoader classLoader = filePath.getClass().getClassLoader();
+        file = new File(classLoader.getResource(resourceName).getFile());
+        absolutePath = file.getAbsolutePath();
+        System.out.println("File Path: " + file);
+        System.out.println("AbsolutePath Using ClassLoader: " + absolutePath);
+
+    }
+
+}
---
+++ Java/java-maven-project/src/main/java/ProductTest.java
@@ -0,0 +1,44 @@
+import model.Product;
+import org.apache.juneau.html.HtmlSerializer;
+import org.apache.juneau.json.JsonParser;
+import org.apache.juneau.json.JsonSerializer;
+import org.apache.juneau.parser.ParseException;
+import org.apache.juneau.serializer.SerializeException;
+import org.apache.juneau.xml.XmlSerializer;
+
+public class ProductTest {
+
+    public static void main(String[] args) throws SerializeException, ParseException {
+
+        String[] sellerNames = {"ABC", "XYZ", "PQR"};
+        //Product product = new Product("MacbookPro", 1000, "White", sellerNames);
+        Product product = Product.builder()
+                .name("MacbookPro")
+                .price(1000)
+                .color("White")
+                .sellerName(sellerNames)
+                .build();
+
+        //POJO to JSON
+        JsonSerializer jsonSerializer = JsonSerializer.DEFAULT_READABLE;
+        String jsonProduct = jsonSerializer.serialize(product);
+        System.out.println(jsonProduct);
+
+        //POJO to XML
+        XmlSerializer xmlSerializer = XmlSerializer.DEFAULT_NS_SQ_READABLE;
+        String xmlProduct = xmlSerializer.serialize(product);
+        System.out.println(xmlProduct);
+
+        //POJO to HTML
+        HtmlSerializer htmlSerializer = HtmlSerializer.DEFAULT_SQ_READABLE;
+        String htmlProduct = htmlSerializer.serialize(product);
+        System.out.println(htmlProduct);
+
+        //Deserialization
+        //JSON to POJO
+        JsonParser jsonParser = JsonParser.DEFAULT;
+        Product product1 = jsonParser.parse(jsonProduct, Product.class);
+        System.out.println("Product: " + product1);
+    }
+
+}
